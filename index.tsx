30eb1817c1a7fdb15411aff2ea50690bd8c9fab1
Sampath Gujarathi <66912066+sampathgujarathi@users.noreply.github.com>
2024-04-24 15:09:08 +0530

    Update index.tsx
    Fixed Profile themes

--- index.tsx
+++ index.tsx
@@ -443,7 +443,7 @@ export default definePlugin({
             }
         },
         {
-            find: ".USER_SETTINGS_PROFILE_THEME_ACCENT",
+            find: ".USER_SETTINGS_RESET_PROFILE_THEME",
             replacement: {
                 match: /RESET_PROFILE_THEME}\)(?<=color:(\i),.{0,500}?color:(\i),.{0,500}?)/,
                 replace: "$&,$self.addCopy3y3Button({primary:$1,accent:$2})"
